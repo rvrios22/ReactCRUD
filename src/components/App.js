ab53c11123c2a49ced8e851b8d1f21d6740c4f8e
Rafael Rios <rvrios22@gmail.com>
2022-07-13 13:40:04 -0700

    adding all files and added actions and reducers

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDoList from './Todo';
+import { connect } from 'react-redux';
+import { addTodo, todoTextChanged } from "../actions/index";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
