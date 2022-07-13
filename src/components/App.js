1c09387f07cea999716c85422be2f87c86bd7608
Rafael Rios <rvrios22@gmail.com>
2022-07-13 14:05:27 -0700

    finished App.js and Todo.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,26 @@ import { addTodo, todoTextChanged } from "../actions/index";
 class App extends React.Component {
     render() {
         return (
-            <div></div>
-        )
+            <div>
+                <h3>Todo List:</h3>
+                <ToDoList items={this.props.items} onAddTodo={this.props.onAddTodo} onTodoTextChanged={this.props.onTodoTextChanged} addTodoText={this.props.addTodoText} />
+            </div>
+        );
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return {
+        onAddTodo: () => dispatch(addTodo()),
+        onTodoTextChanged: text => dispatch(todoTextChanged(text))
+    };
+}
+
+function mapStateToProps(state) {
+    return {
+        items: state.items,
+        addTodoText
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoList = ({ items, onAddTodo, onTodoTextChanged, addTodoText }) => {
+    const todoToListItem = item => <li key={item.id}>{item.todo}</li>;
+
+    const itemList = items.map(todoToListItem);
+
+    return (
+        <div>
+            <input onChange={even => onTodoTextChanged(even.target.value)} value={addTodoText} type="text"/>
+            <button onClick={onAddTodo}>Add Item</button>
+          <ul>{itemList}</ul>
+        </div>
+    );
+};
+
+export default TodoList;
